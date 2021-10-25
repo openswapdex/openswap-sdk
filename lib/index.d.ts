52486c8b2e09f4ad71f4fe69c06a1aab424cd19d
instajs <ycwong@instajs.com>
2021-10-25 14:03:09 +0800

    update index exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
+export { OSWAP_Pair } from './contracts/amm/OSWAP_Pair';
+export { OSWAP_OraclePair } from './contracts/oracle/OSWAP_OraclePair';
 export { OSWAP_Factory } from './contracts/amm/OSWAP_Factory';
 export { deploy } from './deploy';
