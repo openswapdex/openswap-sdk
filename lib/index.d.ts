2807c718bd560aebfd81dabe016412f1b09152f6
benho-ijs <benho@instajs.com>
2021-10-28 00:29:39 +0800

    update index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,2 @@
-export { OSWAP_Pair } from './contracts/amm/OSWAP_Pair';
-export { OSWAP_OraclePair } from './contracts/oracle/OSWAP_OraclePair';
 export * as Contracts from './contracts';
 export { deploy, IDeploymentResult, IDeploymentContracts, toDeploymentContracts } from './deploy';
