b8a0680ed1ba5bb00ef2b6db7206180673adf0e8
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2022-01-19 19:31:08 +0800

    Remove unused files

--- types/contracts/libraries/SafeMath.d.ts
+++
@@ -1,7 +0,0 @@
-import { Wallet, Contract } from "@ijstech/eth-wallet";
-export declare class SafeMath extends Contract {
-    constructor(wallet: Wallet, address?: string);
-    deploy(): Promise<string>;
-}
-export declare module SafeMath {
-}
--- types/contracts/libraries/UQ112x112.d.ts
+++
@@ -1,7 +0,0 @@
-import { Wallet, Contract } from "@ijstech/eth-wallet";
-export declare class UQ112x112 extends Contract {
-    constructor(wallet: Wallet, address?: string);
-    deploy(): Promise<string>;
-}
-export declare module UQ112x112 {
-}
