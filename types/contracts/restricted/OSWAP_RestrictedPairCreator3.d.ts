11302e75196d63f0f8323decaf2c141cb4178488
benho-ijs <benho@instajs.com>
2023-12-15 12:24:21 +0800

    rebuild contracts

---
+++ types/contracts/restricted/OSWAP_RestrictedPairCreator3.d.ts
@@ -0,0 +1,11 @@
+import { IWallet, Contract as _Contract, TransactionReceipt, TransactionOptions } from "@ijstech/eth-contract";
+export declare class OSWAP_RestrictedPairCreator3 extends _Contract {
+    static _abi: any;
+    constructor(wallet: IWallet, address?: string);
+    deploy(options?: TransactionOptions): Promise<string>;
+    createPair: {
+        (salt: string, options?: TransactionOptions): Promise<TransactionReceipt>;
+        call: (salt: string, options?: TransactionOptions) => Promise<string>;
+    };
+    private assign;
+}
